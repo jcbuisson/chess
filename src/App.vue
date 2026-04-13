bd333fcba1372a86972c748db02b42c3144948d3
JC BUISSON <buisson@enseeiht.fr>
2026-04-13 07:31:58 +0200

    WIP

--- src/App.vue
+++ src/App.vue
@@ -20,7 +20,7 @@
                   Reset
                </button>
                <button class="px-4 py-2 bg-gray-700 text-white rounded hover:bg-gray-600 whitespace-nowrap">
-                  Reverse
+                  Revert
                </button>
             </div>
          </div>
