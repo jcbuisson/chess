bbc0655ad9f7925747e43ec1c39aa8c16b1bfec8
JC BUISSON <buisson@enseeiht.fr>
2026-04-13 08:56:10 +0200

    WIP

---
+++ src/chess.worker.js
@@ -0,0 +1,18 @@
+import { createInitialBoard, chessPossibleMoves, moveToString, moveResultingChess, alphabeta, chessBestMove } from '/asscript/build/release.js'
+
+self.onmessage = ({ data: { moveHistory, depth } }) => {
+   let chess = createInitialBoard()
+   let isWhite = true
+
+   for (const moveStr of moveHistory) {
+      const moves = chessPossibleMoves(chess, isWhite)
+      const move = moves.find(m => moveToString(m) === moveStr)
+      chess = moveResultingChess(move)
+      isWhite = !isWhite
+   }
+
+   // isWhite is now the computer's color
+   alphabeta(chess, depth, isWhite)
+   const bestMove = chessBestMove(chess)
+   self.postMessage({ bestMoveStr: moveToString(bestMove) })
+}
